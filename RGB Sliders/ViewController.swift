7186070e96cc52c0930fc95e6ef64ec9aa16d472
Jeffrey Jones <jeff@electriccitystudios.com>
2015-02-09 17:57:53 -0500

    Initial Commit

---
+++ RGB Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Jeffrey Jones on 2/9/15.
+//  Copyright (c) 2015 Electric City Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
